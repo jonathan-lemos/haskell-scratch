1b0c97319f683ed02fd0e5ddd7390cac5ff4dc3f
Jonathan Lemos <jonathanlemos@protonmail.com>
2021-08-10 21:44:17 -0400

    ninth commit

---
+++ reader.hs
@@ -0,0 +1,35 @@
+module ReaderPractice where
+
+import Control.Applicative
+import Data.Maybe
+
+x = [1, 2, 3]
+y = [4, 5, 6]
+z = [7, 8, 9]
+
+xs :: Maybe Integer 
+xs = lookup 3 $ zip x y
+
+ys :: Maybe Integer 
+ys = lookup 6 $ zip y z
+
+zs :: Maybe Integer 
+zs = lookup 4 $ zip x y
+
+z' :: Integer -> Maybe Integer 
+z' = flip lookup $ zip x z
+
+x1 :: Maybe (Integer, Integer)
+x1 = liftA2 (,) xs ys
+
+x2 :: Maybe (Integer, Integer)
+x2 = liftA2 (,) ys zs
+
+x3 :: Integer -> (Maybe Integer, Maybe Integer)
+x3 = liftA2 (,) z' z'
+
+summed :: Num c => (c, c) -> c
+summed = uncurry (+)
+
+bolt :: Integer -> Bool 
+bolt = liftA2 (&&) (>3) (<8)
